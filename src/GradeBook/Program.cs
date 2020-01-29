00006052ac3de7f04dcbf9936e1d9d2f9755cfc4
Liz Tiller <elizabeth.tiller@gmail.com>
2020-01-29 12:42:49 -0500

    updated

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,7 +9,8 @@ namespace GradeBook
         {
             if (args.Length > 0) {
                 Console.WriteLine($"Hello, {args[0]}!");
-
+            } else {
+                Console.WriteLine("Hello!");
             }
         }
     }
