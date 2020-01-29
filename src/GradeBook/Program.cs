baed9156c6e0e068ec5a770300a92aef39af8fb9
Liz Tiller <elizabeth.tiller@gmail.com>
2020-01-29 12:39:25 -0500

    WIP

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -4,9 +4,17 @@ namespace GradeBook
 {
     class Program
     {
+        // method named Main
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length > 0) {
+                Console.WriteLine($"Hello, {args[0]}!");
+
+            }
         }
     }
 }
+
+
+// dotnet run Liz will only be a parameter for the dotnet CLI
+// dotnet run --Liz makes it a variable for the application
